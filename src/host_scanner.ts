a454484cbceb5fd2b9753a54c1141c368bac4423
Tomáš Lederer <tomas.lederer@green.cz>
2022-09-19 13:29:58 +0200

    add input validation, put scanning functions in one file

--- src/host_scanner.ts
+++ src/host_scanner.ts
@@ -1,13 +1,14 @@
-import ip from "ip"
+import ip, {SubnetInfo} from "ip"
 import snmp from "snmp-native";
 import Bluebird from "bluebird";
 import Host from "./Host.js";
 
 const hostname_oid = [1, 3, 6, 1, 2, 1, 1, 5, 0]
+const ifnumber_oid = [1, 3, 6, 1, 2, 1, 1, 5, 0]
+const ifname_oid = [1, 3, 6, 1, 2, 1, 31, 1, 1, 1, 1]
 
 export default {
-    async scanHosts(cidr: string): Promise<Host[]> {
-        const network = ip.cidrSubnet(cidr)
+    async scanHosts(network: SubnetInfo): Promise<Host[]> {
         const firstLong = ip.toLong(network.firstAddress)
         const lastLong = ip.toLong(network.lastAddress)
 
@@ -27,6 +28,28 @@ export default {
         session.close()
         return hosts
     },
+
+    async getInterfaceInfo(host : Host): Promise<Host> {
+        const session = new snmp.Session()
+        const asyncGetNext = Bluebird.promisify(session.getSubtree, {context: session})
+
+        let oid = ifname_oid
+        try {
+            const response = await asyncGetNext({
+                oid: oid,
+                host: host.ip,
+                community: 'private',
+                timeouts: [100, 50, 50, 50]
+            })
+
+            for (let nic of response) {
+                host.interfaces.push(nic.value)
+            }
+
+        } catch (e) {
+        }
+        return host
+    }
 }
 
 
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
 import host_scanner from "./host_scanner.js"
-import interface_info_loader from "./interface_info_loader.js"
+import ip from "ip";
 
 const cidr = process.argv[2]
-console.log(JSON.stringify(process.argv))
-console.log(cidr)
-const hosts = await host_scanner.scanHosts(cidr)
+
+let network
+try {
+    network = ip.cidrSubnet(cidr)
+} catch (e) {
+    console.error("Invalid input. First argument should be network address in CIDR format, e.g. '10.6.6.0/24'")
+    process.exit(1)
+}
+
+const hosts = await host_scanner.scanHosts(network)
 
 for(let host of hosts)
-    await interface_info_loader.getInterfaceInfo(host)
+    await host_scanner.getInterfaceInfo(host)
 
 console.log(JSON.stringify(hosts))
 hosts.forEach(host => host.print())
